28160d230df64fe9be7727f0f70f1cc5804fcc9e
ochi <ocos54ce@gmail.com>
2024-09-10 23:56:39 +0900

    function.php

---
+++ project/wp-content/themes/sample/functions.php
@@ -0,0 +1,19 @@
+<?php
+// テーマフォルダ内の「style.css」を読み込む場合
+function enqueue_custom_styles() {
+  wp_enqueue_style('custom-styles', get_template_directory_uri() . '/css/styles.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
+
+
+add_action('init', function() {
+  echo '<!-- This is a comment. -->';
+});
+
+add_filter('the_title', function($title) {
+  return '★' . $title;
+});
+
+add_filter('show_admin_bar', '__return_false');
+
+?>
