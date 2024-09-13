8b7f744e33b36490fd9e781503109e53d620c30e
ochi <ocos54ce@gmail.com>
2024-09-13 20:23:34 +0900

    個別投稿ページ

--- project/wp-content/themes/sample/functions.php
+++ project/wp-content/themes/sample/functions.php
@@ -20,4 +20,16 @@ add_action('after_setup_theme', function() {
   add_theme_support( 'title-tag' );
 });
 
+function getEyecatchUrl() {
+  if (has_post_thumbnail()) {
+    $id  = get_post_thumbnail_id();
+    $img = wp_get_attachment_image_src($id, "large");
+  } else {
+    $img = array(get_template_directory_uri() . '/assets/img/about-bg.jpg');
+  }
+
+  return $img[0];
+}
+
 ?>
+
---
+++ project/wp-content/themes/sample/includes/header-single.php
@@ -0,0 +1,8 @@
+<header class="masthead" style="background-image: url('<?php echo getEyecatchUrl(); ?>')">
+      <h1><?php the_title(); ?></h1>
+      <p class="subheading">
+        Posted by
+        <a href="#!"><?php the_author(); ?></a>
+        on <?php the_time('Y年n月j日 g:i a'); ?>
+      </p>
+</header>
