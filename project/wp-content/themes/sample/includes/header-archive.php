536ded96b4679706c247b3804c2854c54775cc98
ochi <ocos54ce@gmail.com>
2024-09-18 00:06:17 +0900

    archive

--- project/wp-content/themes/sample/includes/header-archive.php
+++ project/wp-content/themes/sample/includes/header-archive.php
@@ -3,18 +3,15 @@
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
                 <div class="site-heading">
-                <div class="site-heading">
-                    <?php if(is_category()): ?>
-                        <h1>Category</h1>
-                    <?php elseif(is_date()): ?>
-                        <h1>Date</h1>
-                    　<?php elseif(is_author()): ?>
-                        <h1>Author</h1>
-                    <?php else: ?>
-                        <h1>Tag</h1>
-                    <?php endif; ?>
-                    <span class="subheading"><?php wp_title(''); ?></span>
-                    </div>
+                <?php elseif(is_post_type_archive()): ?>
+                <?php $custumFlg = 1; ?>
+                <h1>カスタム投稿</h1>
+                <?php else: ?>
+                <h1>Tab</h1>
+                <?php endif; ?>
+                <?php if($custumFlg != 1): ?>
+                <span class="subheading"><?php wp_title(''); ?></span>
+                <?php endif; ?>
                 </div>
             </div>
         </div>
